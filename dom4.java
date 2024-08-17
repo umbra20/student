9dd88d83c585a344db64c4fb37b11d5b2f9e1e8d
Jurasik/jurasik <mmount743@gmail.com>
2024-08-17 21:11:27 +0300

    add

---
+++ dom4.java
@@ -0,0 +1,13 @@
+public class dom4 {
+    public static void main(String[] args) {
+        int zero = 1;
+        int five = 50;
+        while (zero <= five) {
+            // zero++;
+            if (zero % 3 == 0) {
+                System.out.println(zero);
+            }
+            zero++;
+        }
+    }
+}
