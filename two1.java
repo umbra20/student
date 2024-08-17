1a1e8208c2dd33edc95b10cfda4c58e5ded0d124
Jurasik/jurasik <mmount743@gmail.com>
2024-08-17 17:32:36 +0300

    add

---
+++ two1.java
@@ -0,0 +1,13 @@
+public class Tro {
+    public static void main(String[] args){
+        int x = 4;
+        int y = 4;
+        if (x % 2 == 0 && y % 2 == 0)
+        {
+            System.out.println( "Удачное совпадение");
+        }
+        else
+            System.out.println("Нет совпадений");
+
+    }
+}
