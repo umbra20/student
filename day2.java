e8ab0d99c238f06dcf201dd09b8091a6ad6b4e1d
umbra20 <76448413+umbra20@users.noreply.github.com>
2024-08-18 18:07:44 +0300

    исправлено day2.java

--- day2.java
+++ day2.java
@@ -1,20 +1,22 @@
-//  день второй, второе задание
 public class day2 {
-    public static void main(String[] args)
-    {
-        int day = 15;
+    public static void main(String[] args) {
+        int day = 32;
         boolean m1 = (day > 0) && (day <= 10);
         boolean m2 = (day > 10) && (day <= 20);
         boolean m3 = (day > 20) && (day <= 31);
-           if (day > 0 && day < 11)
-        {
+        String m4 = "Такой даты нет";
+        if (m1) {
             System.out.println(day + "- первая декада");
-        } else if (day > 10 && day < 21)
-        {
+        } else if (m2) {
             System.out.println(day + "- вторая декада");
-        } else
+        } else if (m3) {
             System.out.println(day + "- третья декада");
+        } else {
+            System.out.println(m4);
+
+
+        }
 
 
     }
-    }
+}
