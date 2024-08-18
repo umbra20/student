dd1e7e6d48597990f320beaddba0faa1815354af
umbra20 <76448413+umbra20@users.noreply.github.com>
2024-08-18 17:21:59 +0300

    исправил two1.java

--- two1.java
+++ two1.java
@@ -1,13 +1,14 @@
-public class Tro {
-    public static void main(String[] args){
-        int x = 4;
-        int y = 4;
-        if (x % 2 == 0 && y % 2 == 0)
-        {
-            System.out.println( "Удачное совпадение");
+public class two1 {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 6;
+        if (x % 2 == 0 && y % 2 == 0) {
+            System.out.println("Удачное совпадение");
+        } else if (x % 3 == 0) {
+            System.out.println("в переменой 'x' - Нет совпадений");
+        } else if (y % 3 == 0) {
+            System.out.println("в переменой 'y' - Нет совпадений");
         }
-        else
-            System.out.println("Нет совпадений");
 
     }
 }
