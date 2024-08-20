df10186c3a12b249ccb40234da18c76df3a303c8
Jurasik/jurasik <mmount743@gmail.com>
2024-08-20 23:02:59 +0300

    Калькулятор простой

---
+++ Calc.java
@@ -0,0 +1,8 @@
+public class Calc {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.sum(7, 8);
+        calculator.min(9, 6);
+        calculator.doubl(8, 8);
+    }
+}
