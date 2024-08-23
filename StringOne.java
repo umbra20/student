48276250fa4e262f67c451f3eeba1f51e8122cb4
Yura <76448413+umbra20@users.noreply.github.com>
2024-08-23 23:23:53 +0300

    Update StringOne.java

--- StringOne.java
+++ StringOne.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
-
+//TODO  Дан массив со строками: String[] stringArray = {"ab", "cd", "ef"}.
+Получите массив символов этих строк: char[] charArray =  {'a', 'b', 'c', 'd', 'e', 'f'}.
 public class StringOne {
 
     public static void main(String[] args) {
