c4769e18f6dd4baa9789aa82015502aa9aa0146f
Yura <76448413+umbra20@users.noreply.github.com>
2024-08-24 23:53:08 +0300

    Update and rename day2.java to LesStroke.java
    Вывод первый и последний символ

---
+++ LesStroke.java
@@ -0,0 +1,9 @@
+public class LesStroke {
+    public static void main(String[] args) {
+        String num = "12345";
+        char sim = num.charAt(0);
+        System.out.print(sim);
+        sim = num.charAt(4);
+        System.out.println(sim);
+    }
+}
