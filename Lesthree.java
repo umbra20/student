d10ad304f34a199663be3ee7d00c9ac7b316fbbd
Yura <76448413+umbra20@users.noreply.github.com>
2024-08-25 00:02:30 +0300

    Update and rename two1.java to Lesthree.java
    Поиск первого нуля

---
+++ Lesthree.java
@@ -0,0 +1,8 @@
+public class Lesthree {
+    public static void main(String[] args) {
+        String num = "1203405";
+        int index = num.indexOf('0');
+        System.out.println(index);
+    }
+}
+
