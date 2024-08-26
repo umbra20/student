7e170cbe623837519d231c82ef400158a517b8ae
Yura <76448413+umbra20@users.noreply.github.com>
2024-08-26 16:53:17 +0300

    Update and rename Calculator.java to Gamer.java
    Класс геймер

---
+++ Gamer.java
@@ -0,0 +1,31 @@
+package encapsulation;
+
+public class Gamer {
+    private String nickname;
+    private boolean isActive;
+
+    public String getNickname(){
+        return nickname;
+    }
+    public boolean isActive(){
+        return isActive;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
