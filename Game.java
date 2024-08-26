d7c46f310d0cd0a17b3567ac87fd6bedb1a93413
Yura <76448413+umbra20@users.noreply.github.com>
2024-08-26 16:52:44 +0300

    Update and rename Calc.java to Game.java
    Геймер в сети

---
+++ Game.java
@@ -0,0 +1,11 @@
+package encapsulation;
+
+public class Game {
+
+   public static void main(String[] args){
+      Gamer gamer = new Gamer();
+      System.out.println(gamer.getNickname());
+      System.out.println(gamer.isActive());
+
+   }
+   }
