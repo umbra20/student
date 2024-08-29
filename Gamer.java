a21c93c5a2366f79e8b5474a9b09b8f9ec211b96
Yura <76448413+umbra20@users.noreply.github.com>
2024-08-29 17:16:02 +0300

    Сделал

--- Gamer.java
+++ Gamer.java
@@ -3,18 +3,18 @@ package encapsulation;
 public class Gamer {
     private String nickname;
     private boolean isActive;
-
-    public String getNickname(){
+  public Gamer(String nickname, boolean isActive) {
+      this.nickname = nickname; // конструктор
+      this.isActive = isActive;
+  }
+    public String getNickname() {// функция
         return nickname;
     }
-    public boolean isActive(){
+
+    public boolean isActive() {
         return isActive;
     }
-
 }
-
-
-
 
 
 
